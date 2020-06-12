10d426788165cf2bfee9f6363d93df86bfdc490c
Daniel Gauvin <danielgauvin01@gmail.com>
2020-06-12 01:27:17 -0400

    added spreadturtles method

--- Tictactoe_turtle.py
+++ Tictactoe_turtle.py
@@ -2,44 +2,73 @@ import turtle
 
 
 def draw_board():
+    drawer.pensize(20)
     for i in range(2):
         drawer.penup()
-        drawer.goto(-300, 100 - 200 * i)
+        drawer.goto(turtle.window_width() / 8 - turtle.window_width() / 4 *
+                    i, turtle.window_height() / 3)
         drawer.pendown()
-        drawer.forward(600)
+        drawer.setheading(270)
+        drawer.forward(turtle.window_height() * 2 / 3)
     for i in range(2):
         drawer.penup()
-        drawer.goto(100 - 200 * i, 300)
+        drawer.goto(turtle.window_height() / 3,
+                    turtle.window_width() / 8 - turtle.window_width() / 4 * i)
         drawer.pendown()
-        drawer.setheading(-90)
-        drawer.forward(600)
+        drawer.setheading(180)
+        drawer.forward(turtle.window_height() * 2 / 3)
+    drawer.pensize(10)
+
+
+def spread_turtles():
+    spread_turtles = []
+    for int in range(9):
+        spread_turtles.append(turtle.Turtle())
+        spread_turtles[int].penup()
+        spread_turtles[int].shape("turtle")
+        spread_turtles[int].color("blue")
+
+    spread_turtles[0].goto(-160, 160)
+    spread_turtles[1].goto(0, 160)
+    spread_turtles[2].goto(160, 160)
+    spread_turtles[3].goto(-160, 0)
+    spread_turtles[4].goto(0, 0)
+    spread_turtles[5].goto(160, 0)
+    spread_turtles[6].goto(-160, -160)
+    spread_turtles[7].goto(0, -160)
+    spread_turtles[8].goto(160, -160)
 
 
 def draw_x(x, y):
     drawer.penup()
-    drawer.goto(x + 45, y + 45)
+    drawer.goto(x, y)
     drawer.pendown()
-    drawer.setheading(-135)
-    drawer.forward(127)
-    drawer.penup()
-    drawer.goto(x - 45, y + 45)
-    drawer.pendown()
-    drawer.setheading(-45)
-    drawer.forward(127)
+    drawer.setheading(45)
+    drawer.backward(45)
+    drawer.forward(90)
+    drawer.backward(45)
+    drawer.left(90)
+    drawer.forward(45)
+    drawer.backward(90)
+    drawer.forward(45)
+    print(str(drawer.xcor()) + " " + str(drawer.ycor()))
 
 
 def draw_o(x, y):
+    drawer.setheading(180)
     drawer.penup()
-    drawer.goto(x - 50, y)
+    drawer.goto(x, y + 50)
     drawer.pendown()
     drawer.circle(50)
 
 
+# GAME START
 drawer = turtle.Turtle()
 drawer.speed(10)
-drawer.pensize(20)
 drawer.hideturtle()
 
 draw_board()
-draw_o(0, 0)
+spread_turtles()
+
+turtle.onscreenclick(draw_x)
 turtle.done()
