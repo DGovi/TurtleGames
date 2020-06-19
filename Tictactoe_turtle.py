76dc7e6deb2e944fa4063773d8c9aa3412e4a4d9
Daniel Gauvin <danielgauvin01@gmail.com>
2020-06-19 01:47:55 -0400

    added logic

--- Tictactoe_turtle.py
+++ Tictactoe_turtle.py
@@ -1,76 +1,167 @@
 import turtle
+import random
+
+# drawing methods
 
 
 def draw_board():
-    drawer.pensize(20)
+    board_drawer = turtle.Turtle()
+    board_drawer.speed(20)
+    board_drawer.pensize(20)
+    board_drawer.color("white")
     for i in range(2):
-        drawer.penup()
-        drawer.goto(turtle.window_width() / 8 - turtle.window_width() / 4 *
-                    i, turtle.window_height() / 3)
-        drawer.pendown()
-        drawer.setheading(270)
-        drawer.forward(turtle.window_height() * 2 / 3)
+        board_drawer.penup()
+        board_drawer.goto(turtle.window_width() / 8 - turtle.window_width() / 4 *
+                          i, turtle.window_height() / 3)
+        board_drawer.pendown()
+        board_drawer.setheading(270)
+        board_drawer.forward(turtle.window_height() * 2 / 3)
     for i in range(2):
-        drawer.penup()
-        drawer.goto(turtle.window_height() / 3,
-                    turtle.window_width() / 8 - turtle.window_width() / 4 * i)
-        drawer.pendown()
-        drawer.setheading(180)
-        drawer.forward(turtle.window_height() * 2 / 3)
-    drawer.pensize(10)
+        board_drawer.penup()
+        board_drawer.goto(turtle.window_height() / 3,
+                          turtle.window_width() / 8 - turtle.window_width() / 4 * i)
+        board_drawer.pendown()
+        board_drawer.setheading(180)
+        board_drawer.forward(turtle.window_height() * 2 / 3)
 
 
-def spread_turtles_out():
-    global spread_turtles
-    spread_turtles = []
-    for int in range(9):
-        spread_turtles.append(turtle.Turtle())
-        spread_turtles[int].penup()
-        spread_turtles[int].shape("turtle")
-        spread_turtles[int].color("blue")
-
-    spread_turtles[0].goto(-160, 160)
-    spread_turtles[1].goto(0, 160)
-    spread_turtles[2].goto(160, 160)
-    spread_turtles[3].goto(-160, 0)
-    spread_turtles[4].goto(0, 0)
-    spread_turtles[5].goto(160, 0)
-    spread_turtles[6].goto(-160, -160)
-    spread_turtles[7].goto(0, -160)
-    spread_turtles[8].goto(160, -160)
+def write_message(message):
+    writing_turtle.clear()
+    writing_turtle.penup()
+    writing_turtle.goto(-300, 300)
+    writing_turtle.pendown()
+    writing_turtle.write(message, font=("arial", 20))
 
 
 def draw_x(x, y):
-    drawer.penup()
-    drawer.goto(x, y)
-    drawer.pendown()
-    drawer.setheading(45)
-    drawer.backward(45)
-    drawer.forward(90)
-    drawer.backward(45)
-    drawer.left(90)
-    drawer.forward(45)
-    drawer.backward(90)
-    drawer.forward(45)
+    x_drawer.penup()
+    x_drawer.goto(x, y)
+    x_drawer.pendown()
+    x_drawer.setheading(45)
+    x_drawer.speed(5)
+    x_drawer.backward(45)
+    x_drawer.forward(90)
+    x_drawer.backward(45)
+    x_drawer.speed(0)
+    x_drawer.left(90)
+    x_drawer.speed(5)
+    x_drawer.forward(45)
+    x_drawer.backward(90)
+    x_drawer.forward(45)
+    x_drawer.speed(0)
 
 
 def draw_o(x, y):
-    drawer.setheading(180)
-    drawer.penup()
-    drawer.goto(x, y + 50)
-    drawer.pendown()
-    drawer.circle(50)
+    o_drawer.setheading(180)
+    o_drawer.penup()
+    o_drawer.goto(x, y + 50)
+    o_drawer.pendown()
+    o_drawer.speed(10)
+    o_drawer.circle(50)
+    o_drawer.speed(0)
 
 
-# GAME START
+def display_board_content(board):
+    drawer.clear()
+    current_element = 0
+
+    for row in range(3):
+        for column in range(3):
+            current_element += 1
+            xcor = -160 + 160 * column
+            ycor = 160 - 160 * row
+            drawer.penup()
+            drawer.goto(xcor, ycor)
+            drawer.pendown()
+            if board[current_element] == "x":
+                draw_x(xcor, ycor)
+                continue
+            elif board[current_element] == "o":
+                draw_o(xcor, ycor)
+                continue
+            drawer.write(
+                current_board[current_element], font=("Arial", 16))
+
+
+# game methods
+def play_game(board):
+    display_board_content(board)
+    game_is_finished = False
+    turn = 0
+
+    while not game_is_finished:
+        thisTurn(board)
+        game_is_finished = is_winning_move(board) or turn == 8
+        if turn == 8:
+            write_message("Game is tied")
+        turn += 1
+        display_board_content(board)
+
+
+def thisTurn(board):
+    global turn
+    if turn % 2 == 0:
+        write_message("Player 1".upper())
+        inputted_num = int(the_screen.numinput("Place your symbol",
+                                               "Where would you like to palce your symbol? (one of the numbers shown)", default=None, minval=1, maxval=9))
+        board[inputted_num] = 'x'
+    else:
+        write_message("Player 2".upper())
+        inputted_num = int(the_screen.numinput("Place your symbol",
+                                               "Where would you like to palce your symbol? (one of the numbers shown)", default=None, minval=1, maxval=9))
+        board[inputted_num] = 'o'
+    turn += 1
+
+
+def is_winning_move(board):
+        # check winning possibilities
+    if (board[1] == board[2] == board[3]
+        or board[4] == board[5] == board[6]
+        or board[7] == board[8] == board[9]
+        or board[1] == board[4] == board[7]
+        or board[2] == board[5] == board[8]
+        or board[3] == board[6] == board[9]
+        or board[1] == board[5] == board[9]
+            or board[3] == board[5] == board[7]):
+
+        write_message("winner!!")
+        return True
+    else:
+        return False
+
+
+# game time
+current_board = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+turn = 0
+
+the_screen = turtle.Screen()
+the_screen.title("Tic Tac Toe")
+the_screen.bgcolor("black")
+
 drawer = turtle.Turtle()
-drawer.speed(10)
+drawer.speed(0)
 drawer.hideturtle()
+drawer.color("white")
+
+writing_turtle = turtle.Turtle()
+writing_turtle.speed(0)
+writing_turtle.hideturtle()
+writing_turtle.color("white")
+
+x_drawer = turtle.Turtle()
+x_drawer.color("green")
+x_drawer.pensize(10)
+x_drawer.speed(0)
+x_drawer.hideturtle()
+
+o_drawer = turtle.Turtle()
+o_drawer.color("blue")
+o_drawer.pensize(10)
+o_drawer.speed(0)
+o_drawer.hideturtle()
 
 draw_board()
-spread_turtles_out()
+play_game(current_board)
 
-spread_turtles[0].onclick(draw_x)
 
-turtle.done()
 turtle.exitonclick()
